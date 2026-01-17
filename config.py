fd648fa827456af258ec93679fe95ce71535746f
BtwToxic <btwalexxd@gmail.com>
2026-01-17 17:54:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Config:
         self.MONGO_URL = "mongodb+srv://Krishna:pss968048@cluster0.4rfuzro.mongodb.net/?retryWrites=true&w=majority"
 
         self.LOGGER_ID = -1002706259304
-        self.OWNER_ID = 7953559026
+        self.OWNER_ID = 8319024738
 
         self.DURATION_LIMIT = int(getenv("DURATION_LIMIT", 600000000)) * 600000000
         self.QUEUE_LIMIT = int(getenv("QUEUE_LIMIT", 20000000))
